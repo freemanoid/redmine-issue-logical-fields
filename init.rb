39c066ed4b06b0d22e24f13fbd73597df5088960
freemanoid321 <freemanoid321@gmail.com>
2013-06-02 15:47:31 +0300

    Init plugin.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issue_logical_fields do
+  name 'Issue Logical Fields plugin'
+  author 'Alexandr Elhovenko'
+  description 'Add logical issue custom fields '
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'alexandr.elhovenko@gmail.com'
+end
