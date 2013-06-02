a6b32aba9ede1b09a3f8acf2eb454ef4ef1703fd
freemanoid321 <freemanoid321@gmail.com>
2013-06-03 00:15:44 +0300

    - fix comparison type - change view helpers from tag to form's

--- lib/query_custom_field_column_patch.rb
+++ lib/query_custom_field_column_patch.rb
@@ -6,10 +6,10 @@ module QueryCustomFieldColumnPatch
   module InstanceMethods
     def value_with_compare_type(issue)
       if @cf.field_format == 'compare'
-        operation = %w(< > <= >= =).map(&:to_sym)[@cf.operation_id]
+        operation = %w(< > <= >= ==).map(&:to_sym)[@cf.operation_id]
         return nil unless operation
-        first_date = issue.custom_values.where(custom_field_id: @cf.first_date_id).first.value
-        second_date = issue.custom_values.where(custom_field_id: @cf.second_date_id).first.value
+        first_date = CustomField.find(@cf.first_date_id).cast_value(issue.custom_values.where(custom_field_id: @cf.first_date_id).first.value)
+        second_date = CustomField.find(@cf.second_date_id).cast_value(issue.custom_values.where(custom_field_id: @cf.second_date_id).first.value)
         if first_date.send(operation, second_date)
           @cf.true_message
         else
