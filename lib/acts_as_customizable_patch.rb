210590c51a337f705a559e4d59a98de9cc44336d
freemanoid321 <freemanoid321@gmail.com>
2013-06-06 15:03:37 +0300

    Fix case when date fields are empty.

--- lib/acts_as_customizable_patch.rb
+++ lib/acts_as_customizable_patch.rb
@@ -7,9 +7,12 @@ module LogicalFields
     module InstanceMethods
       def custom_field_values_with_compare_type
         @custom_field_values ||= custom_field_values_without_compare_type.collect do |field|
-          if field.custom_field.field_format == 'compare' && (operation = %w(< > <= >= ==).map(&:to_sym)[field.custom_field.operation_id])
-            first_date = CustomField.find(field.custom_field.first_date_id).cast_value(custom_values.where(custom_field_id: field.custom_field.first_date_id).first.value)
-            second_date = CustomField.find(field.custom_field.second_date_id).cast_value(custom_values.where(custom_field_id: field.custom_field.second_date_id).first.value)
+          if (field.custom_field.field_format == 'compare') &&
+             (operation = %w(< > <= >= ==).map(&:to_sym)[field.custom_field.operation_id]) &&
+             (cv1 = custom_values.where(custom_field_id: field.custom_field.first_date_id)).present? &&
+             (cv2 = custom_values.where(custom_field_id: field.custom_field.second_date_id)).present?
+            first_date = CustomField.find(field.custom_field.first_date_id).cast_value(cv1.first.value)
+            second_date = CustomField.find(field.custom_field.second_date_id).cast_value(cv2.first.value)
             if first_date.send(operation, second_date)
               field.value = field.custom_field.true_message
             else
